ca2e3e6fd6d1507eba0bec6c3289cc164883174e
Noorulhaq Ghafoori <noorulhaq.gha@lvjusd.k12.ca.us>
2019-11-01 11:57:57 -0700

    vjgjjgjhgj

---
+++ newProjeecttttttt/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace newProjeecttttttt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::newProjeecttttttt.Properties.Resources.marioo;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 372);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::newProjeecttttttt.Properties.Resources.hi;
+            this.pictureBox2.Location = new System.Drawing.Point(1287, 372);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(109, 135);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(237, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(810, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(387, 52);
+            this.label2.TabIndex = 3;
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(61, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(264, 23);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 100;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(1132, 31);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(264, 23);
+            this.progressBar2.TabIndex = 5;
+            this.progressBar2.Value = 100;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1316, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kratos";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(659, 562);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::newProjeecttttttt.Properties.Resources.mario_bacckground1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1459, 624);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ newProjeecttttttt/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace newProjeecttttttt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+        }
+        Boolean kr = false;
+        Boolean kl = false;
+        bool mr = false;
+        bool ml = false;
+        int marioJumpdistance = 25;
+
+
+
+
+
+        int kratosJumpdistance = 25;
+
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+
+            //Marioo
+            if (e.KeyCode == Keys.D)
+            {
+                mr = true;
+                ml = false;
+
+                pictureBox1.Image = Properties.Resources.marioo;
+
+                //Point myPoint = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y + 0);
+                //pictureBox1.Location = myPoint;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                ml = true;
+                mr = false;
+
+                pictureBox1.Image = Properties.Resources.mariooleft;
+                //Point myPoint = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 0);
+                // pictureBox1.Location = myPoint;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+
+                if (marioJumpdistance <= -26) //jump is over
+                {
+                    marioJumpdistance = 25;
+                }
+
+                // Point myPoint = new Point(pictureBox1.Location.X + 0, pictureBox1.Location.Y - 100);
+                // pictureBox1.Location = myPoint;
+                timer1.Start();
+            }
+
+
+
+
+            //kratos
+
+            if (e.KeyCode == Keys.NumPad3)
+            {
+
+                kr = true;
+
+
+                kl = false;
+
+                pictureBox2.Image = Properties.Resources.hiright;
+                //Point myPoint = new Point(pictureBox2.Location.X + 5, pictureBox2.Location.Y + 0);
+                //pictureBox2.Location = myPoint;
+            }
+            if (e.KeyCode == Keys.NumPad1)
+            {
+                kl = true;
+                kr = false;
+
+                pictureBox2.Image = Properties.Resources.hi;
+                // Point myPoint = new Point(pictureBox2.Location.X - 5, pictureBox2.Location.Y + 0);
+                // pictureBox2.Location = myPoint;
+            }
+            if (e.KeyCode == Keys.NumPad5)
+            {
+
+                if (kratosJumpdistance <= -26) //jump is over
+                {
+                    kratosJumpdistance = 25;
+                }
+
+                // Point myPoint = new Point(pictureBox1.Location.X + 0, pictureBox1.Location.Y - 100);
+                // pictureBox1.Location = myPoint;
+                timer2.Start();
+
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+
+
+             Point myPoint = new Point(pictureBox1.Location.X + 0, pictureBox1.Location.Y - marioJumpdistance);
+             pictureBox1.Location = myPoint;
+             marioJumpdistance -= 1;
+
+             //if (pictureBox1.Bounds.IntersectsWith(label1.Bounds))// mario collide with brick1
+             //{
+             //    if (marioJumpdistance < 0)//mario going down
+             //    {
+             //        marioJumpdistance = -26;
+             //    }
+             //}
+
+             //timer4.Start();
+
+             if (marioJumpdistance <= -26)
+             {
+                 timer1.Stop();
+             }
+
+
+        }
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+
+
+
+
+            Point myPoint = new Point(pictureBox2.Location.X + 0, pictureBox2.Location.Y - kratosJumpdistance);
+            pictureBox2.Location = myPoint;
+            kratosJumpdistance -= 1;
+
+
+            if (kratosJumpdistance <= -26)
+            {
+                timer2.Stop();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (mr == true && pictureBox1.Location.X < 1390)
+            {
+                Point myPoint = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y + 0);
+                pictureBox1.Location = myPoint;
+            }
+
+
+            if (ml == true && pictureBox1.Location.X > -10)
+            {
+                Point myPoint = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 0);
+                pictureBox1.Location = myPoint;
+            }
+            if (kr == true && pictureBox2.Location.X < 1390)
+            {
+                Point myPoint = new Point(pictureBox2.Location.X + 5, pictureBox2.Location.Y + 0);
+                pictureBox2.Location = myPoint;
+            }
+
+
+            if (kl == true && pictureBox2.Location.X > -10)
+            {
+                Point myPoint = new Point(pictureBox2.Location.X - 5, pictureBox2.Location.Y + 0);
+                pictureBox2.Location = myPoint;
+            }
+
+
+
+
+
+
+            //block collison checks
+
+
+            //collision checks for marrio and ktratos
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                if (pictureBox1.Location.X < pictureBox2.Location.X)  //Mario is more to the left
+                {
+                    Point myPoint = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 0);
+                    pictureBox1.Location = myPoint;
+                }
+                else  //Mario is more to the right
+                {
+                    Point myPoint = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y + 0);
+                    pictureBox1.Location = myPoint;
+                }
+                if (pictureBox2.Location.X < pictureBox1.Location.X)
+                {
+                    Point myPoint = new Point(pictureBox2.Location.X - 5, pictureBox2.Location.Y + 0);
+                    pictureBox2.Location = myPoint;
+                }
+                else
+                {
+                    Point myPoint = new Point(pictureBox2.Location.X + 5, pictureBox2.Location.Y + 0);
+                    pictureBox2.Location = myPoint;
+                }
+
+            }
+
+
+
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                mr = false;
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                ml = false;
+            }
+            if (e.KeyCode == Keys.NumPad3)
+            {
+                kr = false;
+            }
+
+            if (e.KeyCode == Keys.NumPad1)
+            {
+                kl = false;
+            }
+
+
+
+        }
+
+         private void timer4_Tick(object sender, EventArgs e)
+         {
+             if (pictureBox1.Bounds.IntersectsWith(label1.Bounds))
+             {
+                 if (marioJumpdistance !=  24)
+                 {
+                     marioJumpdistance = -24;
+                 }
+             }
+    
+        }
+        int mhealth = 100;
+        
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            mhealth -= 20;
+            progressBar1.Value = mhealth;
+            if(mhealth ==0)
+            {
+                MessageBox.Show("Kratos Wins");
+            }
+        }
+        int khealth = 100;
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+            khealth -= 20;
+            progressBar2.Value = khealth;
+            if (khealth == 0)
+            {
+                MessageBox.Show("Mario Wins");
+            }
+        }
+    }
+}
